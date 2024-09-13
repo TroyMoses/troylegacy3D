7412e75a1b53a8b67599231e43ff55588cd51f54
TroyMoses <mugabimoses07@gmail.com>
2024-09-13 11:58:05 +0800

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I'm a dynamic software developer with a knack for TypeScript and JavaScript, specializing in powerful frameworks like React, Node.js, and Three.js. I{"’"}m a fast learner with a passion for turning complex challenges into elegant, scalable, and user-centric solutions. Let's team up and transform your vision into reality!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
